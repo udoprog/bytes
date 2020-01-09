79371ea9b6163daf1fa270c4a02035f31cc20012
Deployment Bot (from Azure Pipelines) <deploy@tokio-rs.com>
2020-01-09 22:36:18 +0000

    Deploy Bytes API documentation

---
+++ doc/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["bytes"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>",synthetic:false,types:["bytes::bytes_mut::BytesMut"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"bytes/struct.Bytes.html\" title=\"struct bytes::Bytes\">Bytes</a>",synthetic:false,types:["bytes::bytes::Bytes"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bytes"] = {"name":"","dirs":[{"name":"buf","dirs":[{"name":"ext","files":["chain.rs","limit.rs","mod.rs","reader.rs","take.rs","writer.rs"]}],"files":["buf_impl.rs","buf_mut.rs","iter.rs","mod.rs","vec_deque.rs"]}],"files":["bytes.rs","bytes_mut.rs","debug.rs","hex.rs","lib.rs","loom.rs"]};
+createSourceSidebar();
